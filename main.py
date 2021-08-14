ef89c3d72a3f159abf49e3a86ba432dda6212f5c
piaolaidelangman <707056129@qq.com>
2021-08-14 15:24:29 +0800

    20210814 七夕更新

--- main.py
+++ main.py
@@ -14,16 +14,7 @@ num_show_char=3         #每个像素显示次数
 char_num=26
 picnum=0
 def img_to_char(img):
-    # img = cv2.resize(img, (72,96))
-    # print(np.shape(img))
-    # return 
     height, width = np.shape(img)
-    # global picnum
-    # # picnum=0
-    # # img=img.swapaxes(0,1)
-    # if picnum<10:
-    #     cv2.imwrite('movie/'+str(picnum)+'.jpg',np.array(img))
-    #     picnum+=1
     show_str=''
     for i in range(0,height):
         if i>80:
@@ -35,8 +26,6 @@ def img_to_char(img):
             for k in range(num_show_char):
                 show_str+=pix[int(img[i][j])]
             # print(pix[19-img[i][j]],end='')   #倒转  
-            # print(pix[int(img[i][j])]+pix[int(img[i][j])],end='')     #双倍
-            # print(pix[int(img[i][j])]+pix[int(img[i][j])]+pix[int(img[i][j])],end='')
         print(show_str)
             # print(chr(img[i][j]),end='')
     time.sleep(0.1)
@@ -68,10 +57,6 @@ def zip_pic_mean(img):
     img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #480 640
     height, width = np.shape(img)
     new_img=[]
-    # global picnum
-    # if picnum<10:
-    #     cv2.imwrite('movie/'+str(picnum*10)+'.jpg',np.array(img))
-    #     picnum+=1
     i=0
     while i<height:
         row=[]
@@ -101,11 +86,8 @@ def open_by_camera():
     while(cap.isOpened()):
         ret,frame = cap.read()
         if ret == True:
-            # img_to_char(frame)
             # img_to_char(zip_pic_maxpooling(frame))
             img_to_char(zip_pic_mean(frame))
-
-            # cv2.imwrite('capture/'+str(i)+'.jpg',frame)
             i=i+1
         else:
             break
@@ -113,7 +95,6 @@ def open_by_camera():
 def open_by_file():
     ##读取视频文件
     videoCapture = cv2.VideoCapture("newout.mp4")#1080 1920  
-    # videoCapture = cv2.VideoCapture("movie.mp4")# 720 960
     #读帧
     success, img = videoCapture.read()
     i = 0
@@ -126,19 +107,3 @@ def open_by_file():
 if __name__ == '__main__':
     open_by_camera()
     # open_by_file()
-
-
-
-
-
-
-'''
-    print(cap.get(CV_CAP_PROP_FRAME_WIDTH))
-    print(cap.get(CV_CAP_PROP_FRAME_HEIGHT))
-    print(cap.get(CV_CAP_PROP_FPS))
-    print(cap.get(CV_CAP_PROP_BRIGHTNESS))
-    print(cap.get(CV_CAP_PROP_CONTRAST))
-    print(cap.get(CV_CAP_PROP_SATURATION))
-    print(cap.get(CV_CAP_PROP_HUE))
-    print(cap.get(CV_CAP_PROP_EXPOSURE))
-'''
